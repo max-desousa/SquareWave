796e3f4661deef1f049dc9ffe13d45e815a8a2b9
max-desousa <max.de.sousa@outlook.com>
2023-04-09 02:35:09 -0400

    Initializing my squarewave project

---
+++ main.c
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * File:
+ *
+ * Author:
+ *
+ * Description:
+ ******************************************************************************/
+
+/* Include needed std libraries */
+#include <stdio.h>
+
+/* Include micro tools libraries */
+#include "microtools/dcmotor.h"
+
+int main()
+  {
+  printf("Hello, World!\n");
+  return 0;
+  }
